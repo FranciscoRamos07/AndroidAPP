eebca3ed29a62c28e4c8556f2cf485f56a94ceeb
Francisco Ramos <franciscoramos0709@gmail.com>
2023-09-25 21:09:35 -0700

    Initial commit

---
+++ app/src/main/java/com/francisco/coyote/MainActivity.java
@@ -0,0 +1,72 @@
+package com.francisco.coyote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Declaracion de variables
+    TextView holamundo;
+    Button btnExplicito;
+    Button btnExplicit;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("holamundo","onCreate");
+
+        //inicializacion de variables y enlace de componentes
+        holamundo = findViewById(R.id.txtHolaMundo);
+        btnExplicito = findViewById(R.id.btnExplicito);
+        btnExplicit = findViewById(R.id.btnExplicit);
+
+        //evento click
+        btnExplicito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+                //finish(); se utiliza para destruir la actv en contexto
+            }
+        });
+
+
+        btnExplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=El+padrino&oq=El+padrino+&gs_lcrp=EgZjaHJvbWUyDAgAEEUYORixAxiABDIKCAEQABixAxiABDIKCAIQABixAxiABDIKCAMQABixAxiABDIHCAQQABiABDIKCAUQABixAxiABDIGCAYQRRg8MgYIBxBFGDwyBggIEEUYPNIBCDIyNTJqMGoxqAIAsAIA&sourceid=chrome&ie=UTF-8"));
+                startActivity(i);
+                //finish(); se utiliza para destruir la actv en contexto
+            }
+        });
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("holamundo","onStart");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("holamundo","onStart");
+        holamundo.setText("Hola Mundo Desde Clase Java En Metodo OnResume");
+
+
+    }
+}
